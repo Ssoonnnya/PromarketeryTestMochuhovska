a74bb2965d05b7064a4360dd86ae3c72bad5b4a0
Sonya <SofiKonors@gmail.com>
2023-11-10 17:54:10 +0200

    add bootstrap

--- index.view.php
+++ index.view.php
@@ -10,13 +10,15 @@
 
     <link href="https://fonts.cdnfonts.com/css/effra-heavy" rel="stylesheet">
     <link href="https://db.onlinewebfonts.com/c/ca32b369590c4b105ed8cbdf1f8da722?family=GoodDog+New+W00+Regular" rel="stylesheet"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <title>Modal Form</title>
 </head>
-<body>
-<div id="ornament">
-    <nav id="navBar">
-        <div id="logo">
-            <img id="logoIMG" src="style/files/Fav-256px.svg"/>
+<body class="bg-black">
+<div id="ornament" class="container bg-black">
+    <nav id="navBar" class="navbar navbar-expand-md navbar-ligh>
+        <div id = "logo">
+            <img id="logoIMG" src="style/files/Fav-256px.svg" class="img-fluid"/>
         </div>
 
         <div id="logoText"> 
@@ -36,12 +38,15 @@
             <button id="register">Register</button>
         </div>
     </nav>
-    <div><p id="minTabs">Minimize your tabs.</p></div>
-    <div><p id="findTrend">Find the trends!</p></div>
-    <div><p id="parag">Don’t let your computer memories consumes all of those browser tabs. 
-                        Findtrend  let you gathers all of your favorite website into one place.</p></div>
-        <div id="getStarted">
-            <button id="getStartedBtn">Get Started🔥</button>
+    <div class="row">
+        <div class="col-md-6">
+            <div><p id="minTabs">Minimize your tabs.</p></div>
+            <div><p id="findTrend">Find the trends!</p></div>
+            <div><p id="parag">Don’t let your computer memories consumes all of those browser tabs. 
+                                Findtrend  let you gathers all of your favorite website into one place.</p></div>
+            <div class="col-md-6">
+            <div id="getStarted">
+            <button id="getStartedBtn" class="btn btn-primary bg-gray-600">Get Started🔥</button>
 
             <div id="modal" class="modal">
                 <div class="modal-content">
@@ -61,15 +66,22 @@
                 </div>
             </div>
         </div >
-        <div><p id="allResearch">All research start from here</p></div>
-        <img id="arrow" src="style/files/Arrow 03.svg"/>
-        <div id="photos">
-            <img id="twitter" src="style/files/Group 9.svg"/>
-            <img id="pinterest" src="style/files/Group 10.svg"/>
-            <img id="facebook" src="style/files/Group 8.svg"/>
-            <img id="musk" src="style/files/Group 7.svg"/>
-        </div>
     </div>
+</div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div><p id="allResearch">All research start from here</p></div>
+            <img id="arrow" src="style/files/Arrow 03.svg"/>
+                <div id="photos">
+                    <img id="twitter" src="style/files/Group 9.svg" class="img-fluid"/>
+                    <img id="pinterest" src="style/files/Group 10.svg" class="img-fluid"/>
+                    <img id="facebook" src="style/files/Group 8.svg" class="img-fluid"/>
+                    <img id="musk" src="style/files/Group 7.svg" class="img-fluid"/>
+                </div>
+            </div>
+        </div>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="js/script.js"></script>
 </body>
